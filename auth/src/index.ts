ba13f0b873820a3d8a8a0478460b8d7717d6b8a0
Alexandra Astileanu <Alexandra.Astileanu@gmail.com>
2022-10-05 23:54:29 +0300

    Password hashing for users signup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,7 +6,7 @@ import mongoose, { ConnectOptions } from 'mongoose';
 import { currentUserRouter } from './routes/current-user';
 import { signInRouter } from './routes/signin';
 import { signOutRouter } from './routes/signout';
-import { signUpRouter } from './routes/sigup';
+import { signUpRouter } from './routes/signup';
 import { errorHandler } from './middleware/error-handler';
 import { NotFoundError } from './errors/not-found-error';
 import { createLogicalAnd } from 'typescript';
