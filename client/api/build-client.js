de34a0d66d17f3d830823f23f1f41f128253442b
Alexandra Astileanu <alexandra.astileanu@gmail.com>
2022-10-12 22:30:43 +0100

    Display auth status on a header on all pages

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -6,13 +6,13 @@ export default ({ req }) => {
 
     return axios.create({
       baseURL:
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/',
       headers: req.headers,
     });
   } else {
     // We must be on the browser
     return axios.create({
-      baseUrl: '/',
+      baseURL: '/',
     });
   }
 };
