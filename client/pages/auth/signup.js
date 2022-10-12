ce5a1a67d8ede1a1dddae9b8716278fd71cd9bdb
Alexandra Astileanu <alexandra.astileanu@gmail.com>
2022-10-12 17:58:15 +0100

    Server Side Rendering on Index page based on session cookie

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import Router from 'next/router';
 import useRequest from '../../hooks/use-request';
 
 export default () => {
@@ -11,12 +12,15 @@ export default () => {
       email,
       password,
     },
+    onSuccess: () => Router.push('/'),
   });
 
   const onSubmit = async (event) => {
     event.preventDefault();
 
-    doRequest();
+    await doRequest();
+
+    //redirect the use to home page
   };
 
   return (
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,31 @@
-export default () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
   return <h1> Landing Page </h1>;
 };
+
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === 'undefined') {
+    // we are on the server
+    // req should be made to http://ingress-nginx.....
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: req.headers,
+      }
+    );
+    console.log('===Initial props called===');
+
+    return data;
+  } else {
+    //we are on the browser
+    // req can be made with a base url of ''
+
+    const { data } = await axios.get('/api/users/currentuser');
+    console.log('===we are on the browser! ===');
+    return data;
+  }
+};
+
+export default LandingPage;
