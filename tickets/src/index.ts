1516046c9763a079b8e61882cd4faea578bb233e
Alexandra Astileanu <alexandra.astileanu@gmail.com>
2023-01-04 00:35:34 +0200

    Extract MONGO_URI as a constant in k8s deployment files, for auth and tickets

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -4,11 +4,14 @@ import { app } from './app';
 if (!process.env.JWT_KEY) {
   throw new Error('JWT_KEY must be defined');
 }
+if (!process.env.MONGO_URI) {
+  throw new Error('MONGO_URI must be defined');
+}
 
 const start = async () => {
   try {
     //wrong url here...
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+    await mongoose.connect(process.env.MONGO_URI!, {
       useUnifiedTopology: true,
     } as ConnectOptions);
     console.log('Connected to mongodb');
