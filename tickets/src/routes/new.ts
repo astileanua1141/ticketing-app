6bc5fdbc00df017ac277da146e9903b1a3136c1a
Alexandra Astileanu <alexandra.astileanu@gmail.com>
2023-01-04 16:13:10 +0200

    Import current user Route to tickets service, bit of refactoring in test setup

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,8 +1,9 @@
 import express, { Request, Response } from 'express';
+import { requireAuth } from '@alex-asti-demo-org/common';
 
 const router = express.Router();
 
-router.post('/api/tickets', (req, res) => {
+router.post('/api/tickets', requireAuth, (req, res) => {
   res.sendStatus(200);
 });
 
--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -1,5 +1,15 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose, { ConnectOptions } from 'mongoose';
+import request from 'supertest';
+import { app } from '../app';
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin(): Promise<string[]>;
+    }
+  }
+}
 
 let mongo: any;
 
@@ -7,8 +17,11 @@ beforeAll(async () => {
   process.env.JWT_KEY = 'temporarytestkey';
   process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-  mongo = await MongoMemoryServer.create();
-  const mongoUri = mongo.getUri();
+  // mongo = await MongoMemoryServer.create();
+  // const mongoUri = mongo.getUri();
+
+  mongo = new MongoMemoryServer();
+  const mongoUri = await mongo.getUri();
 
   await mongoose.connect(mongoUri, {
     useUnifiedTopology: true,
@@ -27,3 +40,20 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
+
+global.signin = async () => {
+  const email = 'test@test.com';
+  const password = 'password';
+
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email,
+      password,
+    })
+    .expect(201);
+
+  const cookie = response.get('Set-Cookie');
+
+  return cookie;
+};
