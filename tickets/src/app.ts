11bc55ce7592b3435d9137bbae5f32307520df48
Alexandra Astileanu <alexandra.astileanu@gmail.com>
2023-01-20 03:15:14 +0200

    Create the Show ticket route handler

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -9,6 +9,7 @@ import {
 } from '@alex-asti-demo-org/common';
 
 import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
 
 const app = express();
 app.set('trust proxy', true);
@@ -22,6 +23,7 @@ app.use(
 app.use(currentUser);
 
 app.use(createTicketRouter);
+app.use(showTicketRouter);
 
 app.all('/*', async () => {
   throw new NotFoundError();
