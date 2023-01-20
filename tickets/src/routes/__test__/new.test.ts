3c69cbdd78f37bc600cedfaa1a33695050e648d2
Alexandra Astileanu <alexandra.astileanu@gmail.com>
2023-01-20 02:19:40 +0200

    Create a new ticket - route handler

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
 
 it('has a route handler listening to /api/tickets for posts requests', async () => {
   const response = await request(app).post('/api/tickets').send({});
@@ -60,11 +61,22 @@ it('returns an error if an invalid price is provided', async () => {
 
 it('creates a ticket with valid inputs', async () => {
   //add in a check to make sure the ticket was saved
-  // await request(app)
-  //   .post('/api/tickets')
-  //   .send({
-  //     title: 'ABC',
-  //     price: 20,
-  //   })
-  //   .expect(201);
+
+  let tickets = await Ticket.find({});
+
+  expect(tickets.length).toEqual(0);
+
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'ABC',
+      price: 20,
+    })
+    .expect(201);
+
+  tickets = await Ticket.find({});
+
+  expect(tickets.length).toEqual(1);
+  expect(tickets[0].price).toEqual(20);
 });
