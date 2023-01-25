30bc1221688b38f839af1e1520e1830e57642d89
Alexandra Astileanu <alexandra.astileanu@gmail.com>
2023-01-25 17:25:59 +0200

    Extract env variables for nats

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -8,13 +8,23 @@ if (!process.env.JWT_KEY) {
 if (!process.env.MONGO_URI) {
   throw new Error('MONGO_URI must be defined');
 }
+if (!process.env.NATS_CLIENT_ID) {
+  throw new Error('NATS_CLIENT_ID must be defined');
+}
+if (!process.env.NATS_URL) {
+  throw new Error('NATS_URL must be defined');
+}
+if (!process.env.NATS_CLUSTER_ID) {
+  throw new Error('NATS_CLUSTER_ID must be defined');
+}
 
 const start = async () => {
   try {
-    // first argument: ticketing -> the name of the cluster id, specified in the k8s depl as -cid argument
-    // second arg: client id - random chars
-    // third arg: nats srv k8s depl, for client with port 4222
-    await natsWrapper.connect('ticketing', 'efj89j', 'http://nats-srv:4222');
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID!,
+      process.env.NATS_CLIENT_ID!,
+      process.env.NATS_URL!
+    );
 
     natsWrapper.client.on('close', () => {
       console.log('NATS connection closed!');
