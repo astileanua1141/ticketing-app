2291fe321aa314ada03f103cfa1353e3e6e43a6e
Alexandra Astileanu <alexandra.astileanu@gmail.com>
2023-02-19 15:47:53 +0200

    Fetch orders for a user

---
+++ orders/src/routes/__test__/show.test.ts
@@ -0,0 +1,55 @@
+import request from 'supertest';
+import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
+
+it('fetches the order', async () => {
+  // Create a ticket
+  const ticket = Ticket.build({
+    title: 'ticketTitle',
+    price: 20,
+  });
+  await ticket.save();
+
+  const someUser = global.signin();
+
+  // make a request to build an order with this ticket
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', someUser)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // make request to fetch the order
+  const { body: fetchedOrder } = await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set('Cookie', someUser)
+    .send()
+    .expect(200);
+
+  expect(fetchedOrder.id).toEqual(order.id);
+});
+
+it('returns an error if the user tries to fetch another users order', async () => {
+  // Create a ticket
+  const ticket = Ticket.build({
+    title: 'ticketTitle',
+    price: 20,
+  });
+  await ticket.save();
+
+  const someUser = global.signin();
+
+  // make a request to build an order with this ticket
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', someUser)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // make request to fetch the order, as a different user
+  await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set('Cookie', global.signin())
+    .send()
+    .expect(401);
+});
