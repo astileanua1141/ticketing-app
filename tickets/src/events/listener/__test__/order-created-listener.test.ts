50bd9da6368337aa7161e42e83fbe4d8a829be27
Alexandra Astileanu <alexandra.astileanu@gmail.com>
2023-03-05 12:55:24 +0200

    [tickets] Order Created Listener test setup

---
+++ tickets/src/events/listener/__test__/order-created-listener.test.ts
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+import { OrderCreatedEvent, OrderStatus } from '@alex-asti-demo-org/common';
+import { OrderCreatedListener } from '../order-created-listener';
+import { natsWrapper } from '../../../nats-wrapper';
+import { Ticket } from '../../../models/ticket';
+
+const setup = async () => {
+  // Create an instance of the listener
+  const listener = new OrderCreatedListener(natsWrapper.client);
+
+  // Create and save a ticket
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 99,
+    userId: 'asdf',
+  });
+  await ticket.save();
+
+  // Create the fake data event
+  const data: OrderCreatedEvent['data'] = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    version: 1,
+    status: OrderStatus.Created,
+    userId: 'fakeUserId',
+    expiresAt: 'fakeDate',
+    ticket: {
+      id: ticket.id,
+      price: ticket.price,
+    },
+  };
+
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, ticket, data, msg };
+};
